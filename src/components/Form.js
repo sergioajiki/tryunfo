0bba0f54d48b68de8f03c8b9df7c695faa693a53
Sergio <sergioajk@hotmail.com>
2023-01-27 16:22:26 -0300

    requisito 01

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,6 +1,21 @@
 import React from 'react';
 
 class Form extends React.Component {
+  // const {
+  //   cardName,
+  //   cardDescription,
+  //   cardAttr1,
+  //   cardAttr2,
+  //   cardAttr3,
+  //   cardImage,
+  //   cardRare,
+  //   cardTrunfo,
+  //   hasTrunfo,
+  //   isSaveButtonDisabled,
+  //   onInputChange,
+  //   onSaveButtonClick,
+  // } = this.props
+
   render() {
     return (
       <>
@@ -14,25 +29,114 @@ class Form extends React.Component {
               name="name"
               type="text"
               maxLength="40"
+              value=""
               required
             />
           </label>
           <br />
-          <label htmlFor="descricao">
+          <label htmlFor="description">
             Descrição
             <textarea
               data-testid="description-input"
-              id="descricao"
-              name="descricao"
+              id="description"
+              name="description"
               type="text"
               maxLength="500"
+              value=""
               required
             />
           </label>
-
+          <br />
+          <label htmlFor="Attr01">
+            Attr01
+            <input
+              data-testid="attr1-input"
+              id="attr01"
+              name="attr01"
+              type="text"
+              maxLength="40"
+              value=""
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="Attr02">
+            Attr02
+            <input
+              data-testid="attr2-input"
+              id="attr02"
+              name="attr02"
+              type="text"
+              maxLength="40"
+              value=""
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="Attr03">
+            Attr03
+            <input
+              data-testid="attr3-input"
+              id="attr03"
+              name="attr03"
+              type="text"
+              maxLength="40"
+              value=""
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="image">
+            Imagem
+            <input
+              data-testid="image-input"
+              id="image"
+              name="image"
+              type="text"
+              maxLength="40"
+              value=""
+              required
+            />
+          </label>
+          <br />
+          <label htmlFor="rarity">
+            Raridade
+            <select
+              data-testid="rare-input"
+              id="rarity"
+              name="rarity"
+              // type="text"
+              // maxLength="40"
+              value=""
+              required
+            >
+              <option value="normal">normal</option>
+              <option value="raro">raro</option>
+              <option value="muito raro">muito raro</option>
+            </select>
+          </label>
+          <br />
+          <label htmlFor="super-trunfo">
+            Super Trybe Trunfo
+            <input
+              data-testid="trunfo-input"
+              id="super-trunfo"
+              name="super-trunfo"
+              type="checkbox"
+              // maxLength="40"
+              value=""
+              required
+            />
+          </label>
+          <br />
+          <button
+            type="submit"
+            data-testid="save-button"
+          >
+            Salvar
+          </button>
         </form>
       </>
-
     );
   }
 }
